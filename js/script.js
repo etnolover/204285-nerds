8ad49c05c2dc14a22efee9f2c9293198830518ed
Dmitry Lyalin <lyalin.dima@gmail.com>
2016-09-07 19:32:55 +0300

    Сделано всё, кроме: шрифтов, минификации css/js, префиксов к css, также не работает анимация формы. Валидатор выдал warnings, связанный с отсутствием h2-h6 в article и section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+// Карта Google
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
     // center отличается от position, так как в макете маркер
     // находится не в центре карты, а справа чуть ниже центра
-    center: {lat: 59.939090, lng: 30.319899}
+    center: {lat: 59.939230, lng: 30.32}
   });
 
   var image = 'img/marker.png';
@@ -13,3 +14,68 @@ function initMap() {
     icon: image
   });
 }
+
+// Форма 'Напишите нам'
+var link = document.querySelector(".contacts__feedback");
+
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+
+var form = popup.querySelector("form");
+var userName = popup.querySelector("[name='user-name']");
+var userEmail = popup.querySelector("[name='user-email']");
+var userFeedback = popup.getElementsByClassName("feedback-text");
+
+var nameInStorage = localStorage.getItem("userName");
+var emailInStorage = localStorage.getItem("userEmail");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+
+  if (nameInStorage && emailInStorage) {
+    userName.value = nameInStorage;
+    userEmail.value = emailInStorage;
+    userFeedback.focus();
+
+  } else if (nameInStorage && !emailInStorage) {
+    userName.value = nameInStorage;
+    userEmail.focus();
+
+  } else if (!nameInStorage && emailInStorage) {
+    userEmail.value = emailInStorage;
+    userName.focus();
+
+  } else {
+    userName.focus();
+  }
+
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(event) {
+  if (!userName.value || !userEmail.value || !userFeedback.value) {
+    event.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+
+  } else {
+    localStorage.setItem("userName", userName.value);
+    localStorage.setItem("userEmail", userEmail.value);
+  }
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
